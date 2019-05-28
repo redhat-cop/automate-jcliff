fc37cfdc5b24d33a71ac68a207ad32bb1ff07175
Pelisse Romain <belaran@redhat.com>
2019-05-28 15:57:26 +0200

    Tmp fix (#9)
    * bump JCliff default version to latest
    
    * Add a new paramters to define the JVM used to run JCliff
    
    * Ensures jcliff is running with 'bash -x' for debugging purpose
    
    * Populate JCliff environement with now required env variables
    
    * Enhance and improve error handling
    
    * Code cleaning
    
    * Switch local user of Wildfly to 'wildfly'
    
    * Ensures all the basic required packages by Wildfly are installed (mostly in case this is run on Docker)
    
    * Code cleaning
    
    * Add infos for user to know how to test and play with the role
    
    * A symlink is now provided to the lastest version of JCliff
    
    * Add a simple script to help set up a CI job
    
    * Fix typo
    
    * Temporary fix - deploy internal template used by the ansible-jcliff integration to target host
    
    * Fix app deployment
    
    * Enable jcliff testing in molecule
    
    * Ensure 'which' is installed for molecule test
    
    * tweak demo to not use psql driver

---
+++ tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# A simple test suite, designed to be run by a ci-server
+set -e
+
+ansible-playbook -vvvv /work/wildfly-setup.yml
+ansible-playbook -vvvv /work/tune-wildfly-with-jcliff.yml
