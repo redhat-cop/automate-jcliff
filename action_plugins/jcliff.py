4093d2da7778006fb0d8b1c2728ff8adf7780ac4
Pelisse Romain <belaran@redhat.com>
2019-06-14 19:22:54 +0200

    Module remote execution refactor (#10)
    * bump JCliff default version to latest
    
    * Add a new paramters to define the JVM used to run JCliff
    
    * Ensures jcliff is running with 'bash -x' for debugging purpose
    
    * Populate JCliff environement with now required env variables
    
    * Enhance and improve error handling
    
    * Code cleaning
    
    * Switch local user of Wildfly to 'wildfly'
    
    * Ensures all the basic required packages by Wildfly are installed (mostly in case this is run on Docker)
    
    * Code cleaning
    
    * Add infos for user to know how to test and play with the role
    
    * A symlink is now provided to the lastest version of JCliff
    
    * Add a simple script to help set up a CI job
    
    * Fix typo
    
    * Temporary fix - deploy internal template used by the ansible-jcliff integration to target host
    
    * Fix app deployment
    
    * Enable jcliff testing in molecule
    
    * Ensure 'which' is installed for molecule test
    
    * tweak demo to not use psql driver
    
    * Ensure a JVM is installed on the system
    
    * wip
    
    * wip: all tmpl are depoyed
    
    * Disabled facts gathering by default for Wfly tuning playbook
    
    * Remove deployement of internal templates - no longer needed
    
    * Use a remote tmpdir, clean the remote tmp dir plus deploy custom rules
    
    * Makes DS templates more resilient in case some values are not provided
    
    * Code cleanup
    
    * Fixup jdbc driver templates
    
    * Fixup jdbc driver templates
    
    * Rework and fix jdbc-drivers and DS in demo
    
    * misc: enhance example
    
    * misc: update README
    
    * Remove comment line to please yamlint
    
    * Temporary disabling custom_rule
    
    * Avoid failing if no custom rule is provided
    
    * code cleanup

---
+++ action_plugins/jcliff.py
@@ -0,0 +1,66 @@
+# from ansible import utils
+# import ansible.constants as C
+# import ansible.utils.template as template
+# from ansible import errors
+# from ansible.runner.return_data import ReturnData
+
+from ansible.errors import AnsibleError, AnsibleAction, _AnsibleActionDone
+
+import os
+import tempfile
+
+from ansible.plugins.action import ActionBase
+from ansible.template import Templar
+
+class ActionModule(ActionBase):
+
+  TRANSFERS_FILES = True
+  TARGET_FILENAME_SUFFIX = ".jcliff.yml"
+
+  def _writeTemplateResultToFile(self,content):
+    tmp = tempfile.NamedTemporaryFile('w',delete=False)
+    tmp.writelines(content)
+    tmp.close()
+    return tmp.name
+
+  def _templateFromJinjaToYml(self, templateName, subsystem_values):
+    templates = self._loader.path_dwim_relative(self._loader.get_basedir(), 'templates/rules', templateName)
+    with open(templates, 'r') as file:
+      data = file.read()
+    self._templar.set_available_variables(subsystem_values)
+    return self._writeTemplateResultToFile(self._templar.template(data))
+
+  def _deployCustomRulesIfAny(self, tmp_remote_src):
+    if 'rule_file' in self._task.args:
+      custom_rulesdir = self._task.args['rule_file']
+      if custom_rulesdir is not None:
+        for custom_rule_file in os.listdir(custom_rulesdir):
+          self._transfer_file(custom_rulesdir + "/" + custom_rule_file, tmp_remote_src + custom_rule_file + "-custom" + self.TARGET_FILENAME_SUFFIX)
+
+  def _buildAndDeployJCliffRulefiles(self, tmp_remote_src):
+    templateNameBySubsys = {
+      'drivers': 'drivers.j2',
+      'datasources': 'datasource.j2',
+      'system_props': 'system-properties.j2',
+      'deployments': 'deployments.j2'
+    }
+    subsystems = self._task.args['subsystems']
+    for subsys in subsystems:
+      for key in subsys.keys():
+        if key == 'drivers' or key == 'datasources':
+          for index,subsystem_values in enumerate(subsys[key]):
+            self._transfer_file(self._templateFromJinjaToYml(templateNameBySubsys[key], { "values": subsystem_values }), tmp_remote_src + key + "-" + str(index) + self.TARGET_FILENAME_SUFFIX)
+        if key == 'system_props' or key == 'deployments':
+          self._transfer_file(self._templateFromJinjaToYml(templateNameBySubsys[key], { "values": subsys[key]}), tmp_remote_src + key + self.TARGET_FILENAME_SUFFIX)
+
+  def run(self, tmp=None, task_vars=None):
+    tmp_remote_src = self._make_tmp_path()
+    self._buildAndDeployJCliffRulefiles(tmp_remote_src)
+    self._deployCustomRulesIfAny(tmp_remote_src)
+    result = super(ActionModule, self).run(tmp, task_vars)
+    new_module_args = self._task.args.copy()
+    new_module_args.update(dict(remote_rulesdir=tmp_remote_src,))
+    result.update(self._execute_module(module_name='jcliff', module_args=new_module_args, task_vars=task_vars))
+    self._remove_tmp_path(self._connection._shell.tmpdir)
+    return result
+
