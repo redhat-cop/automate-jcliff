293c735c1f3ebf4531c8c9ac281fc998cbbb140d
Pelisse Romain <belaran@gmail.com>
2019-06-21 03:36:16 +0200

    Add some checks to tune-wildfly playbook to ensure jcliff changes has been properly applied (#12)
    * Cleanup CI jobs
    
    * Add validation that jcliff changes were indeed applied to wfly server
    
    * fixup

--- tests.sh
+++ tests.sh
@@ -2,5 +2,7 @@
 # A simple test suite, designed to be run by a ci-server
 set -e
 
+export ANSIBLE_NOCOLOR='True'
 ansible-playbook -vvvv /work/wildfly-setup.yml
-ansible-playbook -vvvv /work/tune-wildfly-with-jcliff.yml --extra-vars "custom_rules_folder=files/custom_rules"
+ansible-playbook -vvvv /work/tune-wildfly-with-jcliff.yml --extra-vars "custom_rules_folder=/work/files/custom_rules"
+rm -rf /work/*
