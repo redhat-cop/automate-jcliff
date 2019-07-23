e9f29989b64727190ab1a24d6c2af3ce4e342215
Romain Pelisse <belaran@gmail.com>
2019-07-23 14:43:19 +0200

    Fix tests.sh

--- tests.sh
+++ tests.sh
@@ -3,7 +3,11 @@
 set -e
 
 export ANSIBLE_NOCOLOR='True'
+
+readonly JCLIFF_ROLE_NAME='redhat-cop.jcliff'
+
 ansible-playbook -vvvv /work/wildfly-setup.yml
-ansible-galaxy install redhat-cop.jcliff
-ansible-playbook -vvvv /work/tune-wildfly-with-jcliff.yml --extra-vars "custom_rules_folder=/work/files/custom_rules"
+ansible-galaxy install "${JCLIFF_ROLE_NAME}"
+ln -s "${HOME}/.ansible/roles/redhat-cop.jcliff" "${JCLIFF_ROLE_NAME}"
+ansible-playbook -vvvv /work/tune-wildfly-with-jcliff.yml --extra-vars "custom_rules_folder=/work/files/custom_rules ansible_distribution=RHEL"
 rm -rf /work/*
