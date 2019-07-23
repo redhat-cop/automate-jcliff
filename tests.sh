6f05db6f4aa158602da7a18df008cc6bb886082c
Romain Pelisse <belaran@gmail.com>
2019-07-23 16:42:46 +0200

    Enhancement and fixes in datasource.j2

--- tests.sh
+++ tests.sh
@@ -5,9 +5,11 @@ set -e
 export ANSIBLE_NOCOLOR='True'
 
 readonly JCLIFF_ROLE_NAME='redhat-cop.jcliff'
+readonly JCLIFF_PLAYBOOK='/work/tune-wildfly-with-jcliff.yml'
 
 ansible-playbook -vvvv /work/wildfly-setup.yml
 ansible-galaxy install "${JCLIFF_ROLE_NAME}"
-ln -s "${HOME}/.ansible/roles/redhat-cop.jcliff" "${JCLIFF_ROLE_NAME}"
-ansible-playbook -vvvv /work/tune-wildfly-with-jcliff.yml --extra-vars "custom_rules_folder=/work/files/custom_rules ansible_distribution=RHEL"
+sed -e "s;- \.;- ${JCLIFF_ROLE_NAME};" -i "${JCLIFF_PLAYBOOK}"
+cat "${JCLIFF_PLAYBOOK}"
+ansible-playbook -vvvv "${JCLIFF_PLAYBOOK}" --extra-vars "custom_rules_folder=/work/files/custom_rules ansible_distribution=CentOS"
 rm -rf /work/*
