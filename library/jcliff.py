6ffd84d89d1dcb18b37cf3df0b6cbf41c46533ce
Romain Pelisse <belaran@gmail.com>
2019-08-09 12:56:38 +0200

    Validation: ensures jcliff_home does exist

--- library/jcliff.py
+++ library/jcliff.py
@@ -150,7 +150,8 @@ def main():
   module = AnsibleModule(argument_spec=fields)
   if os.environ.get("JCLIFF_HOME"):
     fields["jcliff_home"] = os.environ.get("JCLIFF_HOME")
-  # TODO validation that jcliff_home exits + dir
+    if not os.path.exists(fields["jcliff_home"]) or os.path.isfile(fields["jcliff_home"]):
+      raise RuntimeError("Invalid JCLIFF_HOME:" + fields["jcliff_home"])
 
   choice_map = {
       "present": jcliff_present,
