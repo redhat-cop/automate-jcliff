0c8cc729ee780ca9cd015719d8e4b553ed815e3b
Pelisse Romain <belaran@gmail.com>
2019-08-12 12:37:57 +0200

    Code enhancement - fix all the issues reported by Pylint (#20)
    * Fix tests.sh
    
    * Enhancement and fixes in datasource.j2
    
    * code enhancement - fix pylint errors
    
    * pylint: whitespaces and indentation
    
    * pylint: breaking lines too long
    
    * pylint: fix method name to follow snake case convention
    
    * pylint: using 'in' instead of logical operator
    
    * pylint: fixup imports
    
    * pylint: add missing doc_string
    
    * fixup: action module
    
    * pylint: module should not be global
    
    * pylint: remove unnecessary 'else' stmt
    
    * pylint: ensures 'data' is used
    
    * Validation: ensures jcliff_home does exist
    
    * tests.sh - replace ansible-galaxy install with symlink to module repository

--- action_plugins/jcliff.py
+++ action_plugins/jcliff.py
@@ -1,75 +1,79 @@
-# from ansible import utils
-# import ansible.constants as C
-# import ansible.utils.template as template
-# from ansible import errors
-# from ansible.runner.return_data import ReturnData
-
-from ansible.errors import AnsibleError, AnsibleAction, _AnsibleActionDone
-
+""" This action module use JCliff rule templates to
+    render directive before transfering them as rule
+    file to the target host. """
 import os
 import tempfile
 
 from ansible.plugins.action import ActionBase
-from ansible.template import Templar
+
+def _write_template_result_to_file(content):
+  tmp = tempfile.NamedTemporaryFile('w', delete=False)
+  tmp.writelines(content)
+  tmp.close()
+  return tmp.name
+
+# this is temporary workaround, until we figured out a proper way
+# of doing this
+def _get_role_home(plugin_home='/.ansible/roles/redhat-cop.jcliff'):
+  return os.environ['HOME'] + plugin_home
 
 class ActionModule(ActionBase):
-
+  """ JCliff action module """
   TRANSFERS_FILES = True
   TARGET_FILENAME_SUFFIX = ".jcliff.yml"
 
-  def _writeTemplateResultToFile(self,content):
-    tmp = tempfile.NamedTemporaryFile('w',delete=False)
-    tmp.writelines(content)
-    tmp.close()
-    return tmp.name
-
-  # this is temporary workaround, until we figured out a proper way
-  # of doing this
-  def _get_role_home(self):
-    return os.environ['HOME'] + '/.ansible/roles/redhat-cop.jcliff'
-
-  def _templateFromJinjaToYml(self, templateName, subsystem_values):
-    templates = self._loader.path_dwim_relative(self._loader.get_basedir(), 'templates/rules', templateName)
-    if ( not os.path.isfile(templates) ):
-      templates = self._get_role_home() + '/templates/rules/' + templateName
+  def _template_from_jinja_to_yml(self, template_name, subsystem_values):
+    templates = self._loader.path_dwim_relative(self._loader.get_basedir(),
+                                                'templates/rules', template_name)
+    if not os.path.isfile(templates):
+      templates = _get_role_home() + '/templates/rules/' + template_name
 
     with open(templates, 'r') as file:
       data = file.read()
     self._templar.set_available_variables(subsystem_values)
-    return self._writeTemplateResultToFile(self._templar.template(data))
+    return _write_template_result_to_file(self._templar.template(data))
 
-  def _deployCustomRulesIfAny(self, tmp_remote_src):
+  def _deploy_custom_rules_if_any(self, tmp_remote_src):
     if 'rule_file' in self._task.args:
       custom_rulesdir = self._task.args['rule_file']
       if custom_rulesdir is not None:
         for custom_rule_file in os.listdir(custom_rulesdir):
-          self._transfer_file(custom_rulesdir + "/" + custom_rule_file, tmp_remote_src + custom_rule_file + "-custom" + self.TARGET_FILENAME_SUFFIX)
+          self._transfer_file(custom_rulesdir + "/" + custom_rule_file,
+                              tmp_remote_src + custom_rule_file +
+                              "-custom" + self.TARGET_FILENAME_SUFFIX)
 
-  def _buildAndDeployJCliffRulefiles(self, tmp_remote_src):
-    templateNameBySubsys = {
-      'drivers': 'drivers.j2',
-      'datasources': 'datasource.j2',
-      'system_props': 'system-properties.j2',
-      'deployments': 'deployments.j2'
+  def _build_and_deploy_jcliff_rule_files(self, tmp_remote_src):
+    template_name_by_subsys = {
+        'drivers': 'drivers.j2',
+        'datasources': 'datasource.j2',
+        'system_props': 'system-properties.j2',
+        'deployments': 'deployments.j2'
     }
     subsystems = self._task.args['subsystems']
     if subsystems is not None:
       for subsys in subsystems:
         for key in subsys.keys():
-          if key == 'drivers' or key == 'datasources':
+          if key in ('drivers', 'datasources'):
             for index, subsystem_values in enumerate(subsys[key]):
-              self._transfer_file(self._templateFromJinjaToYml(templateNameBySubsys[key], { "values": subsystem_values }), tmp_remote_src + key + "-" + str(index) + self.TARGET_FILENAME_SUFFIX)
-          if key == 'system_props' or key == 'deployments':
-            self._transfer_file(self._templateFromJinjaToYml(templateNameBySubsys[key], { "values": subsys[key]}), tmp_remote_src + key + self.TARGET_FILENAME_SUFFIX)
+              self._transfer_file(
+                  self._template_from_jinja_to_yml(
+                      template_name_by_subsys[key],
+                      {"values": subsystem_values}),
+                  tmp_remote_src + key + "-" +
+                  str(index) + self.TARGET_FILENAME_SUFFIX)
+          if key in ('system_props', 'deployments'):
+            self._transfer_file(self._template_from_jinja_to_yml(
+                template_name_by_subsys[key], {"values": subsys[key]}),
+                                tmp_remote_src + key + self.TARGET_FILENAME_SUFFIX)
 
   def run(self, tmp=None, task_vars=None):
     tmp_remote_src = self._make_tmp_path()
-    self._buildAndDeployJCliffRulefiles(tmp_remote_src)
-    self._deployCustomRulesIfAny(tmp_remote_src)
+    self._build_and_deploy_jcliff_rule_files(tmp_remote_src)
+    self._deploy_custom_rules_if_any(tmp_remote_src)
     result = super(ActionModule, self).run(tmp, task_vars)
     new_module_args = self._task.args.copy()
     new_module_args.update(dict(remote_rulesdir=tmp_remote_src,))
-    result.update(self._execute_module(module_name='jcliff', module_args=new_module_args, task_vars=task_vars))
+    result.update(self._execute_module(
+        module_name='jcliff', module_args=new_module_args, task_vars=task_vars))
     self._remove_tmp_path(self._connection._shell.tmpdir)
     return result
-
--- library/jcliff.py
+++ library/jcliff.py
@@ -1,148 +1,164 @@
 #!/usr/bin/python
-
-from ansible.module_utils.basic import AnsibleModule
-import json
+""" This is a jcliff module, wrapping the utility Java tool JCliff
+    in order to have Ansible supports fine tuning of Wildfly / JBoss
+    EAP server configuration (subsystems)"""
 import subprocess
 import os
-import tempfile
-import shutil
 
-def listRuleFiles(rulesdir):
+from ansible.module_utils.basic import AnsibleModule
+
+def list_rule_files(rulesdir):
+  """ list all the files inside the rule's directory """
   rules_filename = os.listdir(rulesdir)
   rule_files = []
   for filename in rules_filename:
-      if filename.endswith("jcliff.yml"):
-        print("rule file:" + rulesdir + "/" + filename)
-        rule_files.append(rulesdir + "/" + filename)
+    if filename.endswith("jcliff.yml"):
+      print("rule file:" + rulesdir + "/" + filename)
+      rule_files.append(rulesdir + "/" + filename)
   return rule_files
 
-def addToEnv(name, value):
+def add_to_env(name, value):
+  """ add provided variables to environnement """
   if value is not None:
     os.environ[name] = value
 
-def executeRulesWithJCliff(data):
+def execute_rules_with_jcliff(data):
+  """ execute the rules provided using jcliff """
 
-  jcliff_command_line = ["bash", "-x", data["jcliff"], "--cli=" + data['wfly_home'] + "/bin/jboss-cli.sh", "--ruledir=" + data['rules_dir'], "--controller=" + data['management_host'] + ":" + data['management_port'], "-v"]
+  jcliff_command_line = ["bash", "-x",
+                         data["jcliff"], "--cli=" + data['wfly_home'] + "/bin/jboss-cli.sh",
+                         "--ruledir=" + data['rules_dir'],
+                         "--controller=" + data['management_host'] + ":" + data['management_port'],
+                         "-v"]
 
   if data["management_username"] is not None:
     jcliff_command_line.extend(["--user=" + data["management_username"]])
   if data["management_password"] is not None:
     jcliff_command_line.extend(["--password=" + data["management_password"]])
 
-  jcliff_command_line.extend(listRuleFiles(data['remote_rulesdir']))
+  jcliff_command_line.extend(list_rule_files(data['remote_rulesdir']))
   output = None
   status = "undefined"
-  addToEnv('JAVA_HOME', data['jcliff_jvm'])
-  addToEnv('JBOSS_HOME', data['wfly_home'])
-  addToEnv('JCLIFF_HOME', data['jcliff_home'])
+  add_to_env('JAVA_HOME', data['jcliff_jvm'])
+  add_to_env('JBOSS_HOME', data['wfly_home'])
+  add_to_env('JCLIFF_HOME', data['jcliff_home'])
 
   try:
-    output = subprocess.check_output(jcliff_command_line,stderr=subprocess.STDOUT, shell=False, env=os.environ)
+    output = subprocess.check_output(jcliff_command_line,
+                                     stderr=subprocess.STDOUT,
+                                     shell=False,
+                                     env=os.environ)
     status = 0
   except subprocess.CalledProcessError as jcliffexc:
     error = jcliffexc.output.decode()
     if (jcliffexc.returncode != 2) and (jcliffexc.returncode != 0):
-        return { "failed": True, "report": { "status" : jcliffexc.returncode, "output": output, "jcliff_cli": jcliff_command_line , "JAVA_HOME": os.getenv("JAVA_HOME","Not defined"), "JCLIFF_HOME": os.getenv("JCLIFF_HOME", "Not defined"), "JBOSS_HOME": os.getenv("JBOSS_HOME", "Not defined"), "error": error } }, jcliffexc.returncode
-    else:
-        return {"present:": error },2
-  except Exception as e:
-     output = e
-     status = 1
+      return {"failed": True, "report":
+              {"status" : jcliffexc.returncode,
+               "output": output,
+               "jcliff_cli": jcliff_command_line,
+               "JAVA_HOME": os.getenv("JAVA_HOME", "Not defined"),
+               "JCLIFF_HOME": os.getenv("JCLIFF_HOME", "Not defined"),
+               "JBOSS_HOME": os.getenv("JBOSS_HOME", "Not defined"),
+               "error": error}}, jcliffexc.returncode
+    return {"present:": error}, 2
+  except Exception as exception:
+    output = exception
+    status = 1
   if status == 0:
-    return {"present" : output, "rc": status, "jcliff_cli": jcliff_command_line }, status
-  else:
-      return {"failed" : True, "report": output, "rc": status, "jcliff_cli": jcliff_command_line }, status
+    return {"present" : output, "rc": status, "jcliff_cli": jcliff_command_line}, status
+  return {"failed" : True,
+          "report": output,
+          "rc": status,
+          "jcliff_cli": jcliff_command_line}, status
 
-def ansibleResultFromStatus(status):
+def ansible_result_from_status(status):
+  """ extract the ansible result from the status returned by jcliff """
   has_changed = False
   has_failed = False
   if status == 2:
     has_changed = True
-  if status != 0 and status != 2:
+  if status not in (0, 2):
     has_failed = True
   return (has_changed, has_failed)
 
 def jcliff_present(data):
+  """ implement ansible present state using jcliff """
   print("Executing JCliff:")
-  meta, status = executeRulesWithJCliff(data)
-  has_changed, has_failed = ansibleResultFromStatus(status)
+  meta, status = execute_rules_with_jcliff(data)
+  has_changed, has_failed = ansible_result_from_status(status)
   return (has_changed, has_failed, meta)
 
 def jcliff_absent(data=None):
-   has_changed = False
-   #subprocess.run(["ls", "-l"])
-   meta = {"absent": "not yet implemented"}
+  """ jcliff and this module does NOT support the absent state """
+  return {"absent": "not yet implemented", "data": data}
 
 def main():
-    print("JCliff module")
-    default_jcliff_home = "/usr/share/jcliff"
-    fields = dict(
-         jcliff_home=dict(type='str', default=default_jcliff_home),
-         jcliff=dict(default='/usr/bin/jcliff', type='str'),
-         management_username=dict(required=False, type='str'),
-         management_password=dict(required=False, type='str'),
-         rules_dir=dict(type='str', default=default_jcliff_home + "/rules"),
-         wfly_home=dict(required=True, type='str'),
-         management_host=dict(default='localhost', type='str'),
-         management_port=dict(default='9990', type='str'),
-         jcliff_jvm=dict(default=os.getenv("JAVA_HOME",None), type='str', required=False),
-         rule_file=dict(required=False, type='str'),
-         remote_rulesdir=dict(required=False, type='str'), # do not use, value is provided by the actions plugin
-         subsystems=dict(type='list', required=False, elements='dict',
-            options=dict(
-                drivers=dict(type='list', required=False, elements='dict', options=dict(
-                    driver_name=dict(type='str', required=True),
-                    driver_module_name=dict(type='str', required=True),
-                    driver_xa_datasource_class_name=dict(type='str', default='undefined'),
-                    driver_class_name=dict(type='str', default='undefined'),
-                    driver_datasource_class_name=dict(type='str', default='undefined'),
-                    module_slot=dict(type='str', default='undefined'),
-                    )
-                ),
-                datasources=dict(type='list', required=False, elements='dict', options=dict(
-                    name=dict(type='str', required=True),
-                    pool_name=dict(type='str', required=False),
-                    jndi_name=dict(type='str', required=True),
-                    use_java_context=dict(type='str', default='true'),
-                    connection_url=dict(type='str', required=True),
-                    driver_name=dict(type='str', required=True),
-                    enabled=dict(type='str', default='true'),
-                    password=dict(type='str', required=False),
-                    user_name=dict(type='str', required=False),
-		    max_pool_size=dict(type='str', default='undefined'),
-            	    min_pool_size=dict(type='str', default='undefined'),
-	            idle_timeout_minutes=dict(type='str', default='undefined'),
-                    query_timeout=dict(type='str', default='undefined'),
-                    check_valid_connection_sql=dict(type='str', default='undefined'),
-                    validate_on_match=dict(type='str', default='undefined')
-                    )
-                ),
-                system_props=dict(type='list', required=False, elements='dict', options=dict(
-                    name=dict(type='str', required=False),
-                    value=dict(type='str', required=False)
-                    )
-                ),
-                deployments=dict(type='list', required=False, elements='dict', options=dict(
-                    artifact_id=dict(type='str', required=True),
-                    name=dict(type='str', required=False),
-                    path=dict(type='str', required=True)
-                    )
-                )
-            )
-         ),
-         state=dict(default="present", choices=['present', 'absent'], type='str')
+  """ Main method for this module """
+  print("JCliff module")
+  default_jcliff_home = "/usr/share/jcliff"
+  fields = dict(
+      jcliff_home=dict(type='str', default=default_jcliff_home),
+      jcliff=dict(default='/usr/bin/jcliff', type='str'),
+      management_username=dict(required=False, type='str'),
+      management_password=dict(required=False, type='str'),
+      rules_dir=dict(type='str', default=default_jcliff_home + "/rules"),
+      wfly_home=dict(required=True, type='str'),
+      management_host=dict(default='localhost', type='str'),
+      management_port=dict(default='9990', type='str'),
+      jcliff_jvm=dict(default=os.getenv("JAVA_HOME", None), type='str', required=False),
+      rule_file=dict(required=False, type='str'),
+      # do not use the following parameters, value is provided by the actions plugin
+      remote_rulesdir=dict(required=False, type='str'),
+      subsystems=dict(type='list', required=False, elements='dict',
+                      options=dict(
+                          drivers=dict(type='list', required=False, elements='dict', options=dict(
+                              driver_name=dict(type='str', required=True),
+                              driver_module_name=dict(type='str', required=True),
+                              driver_xa_datasource_class_name=dict(type='str', default='undefined'),
+                              driver_class_name=dict(type='str', default='undefined'),
+                              driver_datasource_class_name=dict(type='str', default='undefined'),
+                              module_slot=dict(type='str', default='undefined'))),
+                          datasources=dict(
+                              type='list', required=False, elements='dict',
+                              options=dict(
+                                  name=dict(type='str', required=True),
+                                  pool_name=dict(type='str', required=False),
+                                  jndi_name=dict(type='str', required=True),
+                                  use_java_context=dict(type='str', default='true'),
+                                  connection_url=dict(type='str', required=True),
+                                  driver_name=dict(type='str', required=True),
+                                  enabled=dict(type='str', default='true'),
+                                  password=dict(type='str', required=False),
+                                  user_name=dict(type='str', required=False),
+                                  max_pool_size=dict(type='str', default='undefined'),
+                                  min_pool_size=dict(type='str', default='undefined'),
+                                  idle_timeout_minutes=dict(type='str', default='undefined'),
+                                  query_timeout=dict(type='str', default='undefined'),
+                                  check_valid_connection_sql=dict(type='str', default='undefined'),
+                                  validate_on_match=dict(type='str', default='undefined'))),
+                          system_props=dict(
+                              type='list', required=False, elements='dict', options=dict(
+                                  name=dict(type='str', required=False),
+                                  value=dict(type='str', required=False))),
+                          deployments=dict(
+                              type='list', required=False, elements='dict', options=dict(
+                                  artifact_id=dict(type='str', required=True),
+                                  name=dict(type='str', required=False),
+                                  path=dict(type='str', required=True))))),
+      state=dict(default="present", choices=['present', 'absent'], type='str')
     )
-    global module
-    module = AnsibleModule(argument_spec=fields)
-    if os.environ.get("JCLIFF_HOME"):
-        fields["jcliff_home"] = os.environ.get("JCLIFF_HOME")
-    # TODO validation that jcliff_home exits + dir
+  module = AnsibleModule(argument_spec=fields)
+  if os.environ.get("JCLIFF_HOME"):
+    fields["jcliff_home"] = os.environ.get("JCLIFF_HOME")
+    if not os.path.exists(fields["jcliff_home"]) or os.path.isfile(fields["jcliff_home"]):
+      raise RuntimeError("Invalid JCLIFF_HOME:" + fields["jcliff_home"])
 
-    choice_map = {
-        "present": jcliff_present,
-        "absent": jcliff_absent,
-    }
-    has_changed, has_failed, result = choice_map.get(module.params['state'])(data=module.params)
-    module.exit_json(changed=has_changed, failed=has_failed, meta=result)
+  choice_map = {
+      "present": jcliff_present,
+      "absent": jcliff_absent,
+  }
+  has_changed, has_failed, result = choice_map.get(module.params['state'])(data=module.params)
+  module.exit_json(changed=has_changed, failed=has_failed, meta=result)
+
 if __name__ == '__main__':
-    main()
+  main()
